06957ab2068c3b6f0a06c24c5f8aa9b57a990a25
王福军 <wfj@meitu.com>
2018-08-06 13:13:25 +0800

    【需求】编译so文件，通过c获取ffmpeg信息

---
+++ app/src/main/java/com/meitu/ffmpegandroid/utils/FFUtils.java
@@ -0,0 +1,15 @@
+package com.meitu.ffmpegandroid.utils;
+
+/**
+ * Create By WFJ 2018/8/3
+ */
+public class FFUtils {
+
+    public static native String urlProtocolInfo();
+
+    public static native String avFormatInfo();
+
+    public static native String avCodecInfo();
+
+    public static native String avFilterInfo();
+}
